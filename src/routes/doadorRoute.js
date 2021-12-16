d651bdeefb6ebbf3992b8b3a9bf5b63371578843
22augusta <93688427+22augusta@users.noreply.github.com>
2021-12-16 19:43:00 -0300

    Modo teste

--- src/routes/doadorRoute.js
+++ src/routes/doadorRoute.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const controller = require("../controller/doadorController")
 
 router.get("/", controller.getAllDoador)
-router.get("/:id", controller.getDoador)
+router.get("/:id", controller.getDoadorId)
 router.post("/", controller.createDoador)
 router.delete("/:id", controller.deleteDoador)
 router.put("/:id", controller.updateDoador)
